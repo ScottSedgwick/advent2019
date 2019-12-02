1df213808f3e29b662a7eb166477232e199f0515
Scott Sedgwick <ssedgwick@cochlear.com>
2019-12-02 20:00:59 +1100

    Refactored Day2 to be faster

--- app/Main.hs
+++ app/Main.hs
@@ -1,10 +1,11 @@
 module Main where
 
 import Lib
+import Data.IntMap
 
 main :: IO ()
 main = do
-  print $ head $ opcode input1                      -- 2692315
+  print $ opcode input1 ! 0                         -- 2692315
   print $ a * 100 + b                               -- 9507
   where
     xs = [(x,y) | x <- [0..99], y <- [0..99]]
--- src/Advent/Day2.hs
+++ src/Advent/Day2.hs
@@ -1,38 +1,49 @@
 module Advent.Day2 where
 
-opcode :: [Int] -> [Int]
-opcode = process 0
+import Data.IntMap.Lazy
 
-process :: Int -> [Int] -> [Int]
-process x xs = if stop then xs' else process (x + 4) xs'
-  where
-    (xs', stop) = checkOp (take 4 (drop x xs)) xs
+encode :: Int -> [Int] -> IntMap Int
+encode x = fromAscList . zip [x..]
 
-checkOp :: [Int] -> [Int] -> ([Int], Bool)
-checkOp (99:_) xs = (xs, True)
-checkOp [1,in1,in2,out] xs = (op (+) in1 in2 out xs, False)
-checkOp [2,in1,in2,out] xs = (op (*) in1 in2 out xs, False)
-checkOp _ xs = (xs, True)
+enc :: [Int] -> IntMap Int
+enc = encode 0
 
-op :: (Int -> Int -> Int) -> Int -> Int -> Int -> [Int] -> [Int]
-op o in1 in2 out xs = xs'  
-  where
-    i1 = xs !! in1
-    i2 = xs !! in2
-    xs' = take out xs ++ [o i1 i2] ++ drop (out + 1) xs
+dec :: IntMap Int -> [Int]
+dec = elems
 
-input1 :: [Int]
-input1 = [1,12,2,3,1,1,2,3,1,3,4,3,1,5,0,3,2,1,6,19,1,19,5,23,2,13,23,27,1,10,27,31,2,6,31,35,1,9,35,39,2,10,39,43,1,43,9,47,1,47,9,51,2,10,51,55,1,55,9,59,1,59,5,63,1,63,6,67,2,6,67,71,2,10,71,75,1,75,5,79,1,9,79,83,2,83,10,87,1,87,6,91,1,13,91,95,2,10,95,99,1,99,6,103,2,13,103,107,1,107,2,111,1,111,9,0,99,2,14,0,0]
+opcode :: IntMap Int -> IntMap Int
+opcode = opcode' 0
 
-input2 :: [Int]
-input2 = [3,1,1,2,3,1,3,4,3,1,5,0,3,2,1,6,19,1,19,5,23,2,13,23,27,1,10,27,31,2,6,31,35,1,9,35,39,2,10,39,43,1,43,9,47,1,47,9,51,2,10,51,55,1,55,9,59,1,59,5,63,1,63,6,67,2,6,67,71,2,10,71,75,1,75,5,79,1,9,79,83,2,83,10,87,1,87,6,91,1,13,91,95,2,10,95,99,1,99,6,103,2,13,103,107,1,107,2,111,1,111,9,0,99,2,14,0,0]
+opcode' :: Int -> IntMap Int -> IntMap Int
+opcode' i zs = 
+  case zs ! i of
+    99 -> zs
+    1  -> opcode' (i + 4) (insert (zs ! (i + 3)) (zs ! (zs ! (i + 1)) + (zs ! (zs ! (i + 2)))) zs)
+    2  -> opcode' (i + 4) (insert (zs ! (i + 3)) (zs ! (zs ! (i + 1)) * (zs ! (zs ! (i + 2)))) zs)
+    _  -> zs
 
-inp :: (Int, Int) -> [Int]
-inp (noun, verb) = 1 : noun : verb : input2
+op4 :: Int -> (Int -> Int -> Int) -> IntMap Int -> IntMap Int
+op4 i op zs = insert c v zs
+  where 
+    a = zs ! (i + 1)
+    b = zs ! (i + 2)
+    c = zs ! (i + 3)
+    d = zs ! a
+    e = zs ! b
+    v = op d e
+
+input1 :: IntMap Int
+input1 = inp (12, 2)
+
+input2 :: IntMap Int
+input2 = insert 0 1$ fromAscList $ zip [3..] [3,1,1,2,3,1,3,4,3,1,5,0,3,2,1,6,19,1,19,5,23,2,13,23,27,1,10,27,31,2,6,31,35,1,9,35,39,2,10,39,43,1,43,9,47,1,47,9,51,2,10,51,55,1,55,9,59,1,59,5,63,1,63,6,67,2,6,67,71,2,10,71,75,1,75,5,79,1,9,79,83,2,83,10,87,1,87,6,91,1,13,91,95,2,10,95,99,1,99,6,103,2,13,103,107,1,107,2,111,1,111,9,0,99,2,14,0,0]
+
+inp :: (Int, Int) -> IntMap Int
+inp (noun, verb) = insert 1 noun (insert 2 verb input2)
 
 seek :: [(Int, Int)] -> (Int, Int)
 seek [] = (-1, -1)
 seek (x:xs) =
-  if head (opcode (inp x)) == 19690720
+  if opcode (inp x) ! 0 == 19690720
   then x
   else seek xs
