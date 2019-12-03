51bf7222aa1cc0f89facbf83b88bb70cba08c68b
Scott Sedgwick <ssedgwick@cochlear.com>
2019-12-03 19:04:18 +1100

    Completed Day 3

--- src/Advent/Day3.hs
+++ src/Advent/Day3.hs
@@ -1,4 +1,89 @@
 module Advent.Day3 where
 
-add :: Int -> Int -> Int
-add = (+)
+import Data.List (sortOn, intersect)
+import Data.List.Split (splitOn)
+import qualified Data.IntMap.Strict as M
+
+data Vector
+  = Up Int
+  | Dn Int
+  | Lt Int
+  | Rt Int
+  deriving (Show, Eq)
+
+parse :: String -> [Vector]
+parse = map parseVec . splitOn ","
+
+parseVec :: String -> Vector
+parseVec ('U':xs) = Up (read xs :: Int)
+parseVec ('D':xs) = Dn (read xs :: Int)
+parseVec ('L':xs) = Lt (read xs :: Int)
+parseVec ('R':xs) = Rt (read xs :: Int)
+
+type Posn = Int
+type Steps = Int
+
+toPosn :: (Int, Int) -> Posn
+toPosn (x,y) = x * 10000 + y
+
+fromPosn :: Posn -> (Int, Int)
+fromPosn p = (p `div` 10000, p `mod` 10000)
+
+type Wireboard = M.IntMap Steps
+
+wirepath :: Posn -> Steps -> [Vector] -> Wireboard -> Wireboard
+wirepath _ _ [] wb = wb
+wirepath p s (z:zs) wb = segpath p s z wb `M.union` wirepath (nextpos p z) (nextstep s z) zs wb
+
+segpath :: Posn -> Steps -> Vector -> Wireboard -> Wireboard
+segpath p s (Up n) wb = wb `M.union` M.fromList (map (\z -> (p + z, s + z)) [0..n - 1])
+segpath p s (Dn n) wb = wb `M.union` M.fromList (map (\z -> (p - z, s + z)) [0..n - 1])
+segpath p s (Lt n) wb = wb `M.union` M.fromList (map (\z -> (p - z * 10000, s + z)) [0..n - 1])
+segpath p s (Rt n) wb = wb `M.union` M.fromList (map (\z -> (p + z * 10000, s + z)) [0..n - 1])
+
+nextpos :: Posn -> Vector -> Posn
+nextpos p (Up n) = p + n
+nextpos p (Dn n) = p - n
+nextpos p (Lt n) = p - n * 10000
+nextpos p (Rt n) = p + n * 10000
+
+nextstep :: Steps -> Vector -> Steps
+nextstep x (Up n) = x + n
+nextstep x (Dn n) = x + n
+nextstep x (Lt n) = x + n
+nextstep x (Rt n) = x + n
+
+intersection :: Wireboard -> Wireboard -> Wireboard
+intersection = M.intersectionWith (+)
+
+distance :: [(Int, Int)] -> Int
+distance = dist . fst . nearest order1
+
+order1 :: [(Posn, Steps)] -> [(Posn, Steps)]
+order1 = sortOn (dist . fst)
+
+dist :: Posn -> Int
+dist p = abs x + abs y
+  where 
+    (x,y) = fromPosn p
+
+d3p1 :: String -> String -> Int
+d3p1 x y = distance (M.toList ps)
+  where
+    w1 = wirepath 0 0 (parse x) M.empty
+    w2 = wirepath 0 0 (parse y) M.empty
+    ps = intersection w1 w2
+
+d3p2 :: String -> String -> Steps
+d3p2 s1 s2 = snd x
+  where
+    w1 = wirepath 0 0 (parse s1) M.empty
+    w2 = wirepath 0 0 (parse s2) M.empty
+    ps = intersection w1 w2
+    x  = nearest order2 (M.toList ps)
+
+order2 :: [(Int, Int)] -> [(Int, Int)]
+order2 = sortOn snd
+
+nearest ::([(Int, Int)] -> [(Int, Int)]) -> [(Int, Int)] -> (Int, Int)
+nearest f xs = f xs !! 1
--- test/Advent/Day3.hs
+++ test/Advent/Day3.hs
@@ -1,9 +1,28 @@
 module Advent.Day3 where
 
+import Data.List (intersect)
 import Test.HUnit
 import Lib
+import qualified Data.IntMap.Strict as M
+
+d3tests = [
+    [Rt 8, Up 5, Lt 5, Dn 3] ~=? parse "R8,U5,L5,D3"
+  , [Up 7, Rt 6, Dn 4, Lt 4] ~=? parse "U7,R6,D4,L4"
+  , M.fromList [(0,0), (10000,1), (20000,2), (30000,3), (40000,4)] ~=? wirepath 0 0 [Rt 5] M.empty
+  , M.fromList [(0,0), (60005,30), (30003, 40)] ~=? intersection (wirepath 0 0 (parse "R8,U5,L5,D3") M.empty) (wirepath 0 0 (parse "U7,R6,D4,L4") M.empty)
+  ]
+
+d3p1tests = [
+    6 ~=? d3p1 "R8,U5,L5,D3" "U7,R6,D4,L4"
+  , 135 ~=? d3p1 "R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51" "U98,R91,D20,R16,D67,R40,U7,R15,U6,R7"
+  , 159 ~=? d3p1 "R75,D30,R83,U83,L12,D49,R71,U7,L72" "U62,R66,U55,R34,D71,R55,D58,R83"
+  ]
+
+d3p2tests = [
+    30 ~=? d3p2 "R8,U5,L5,D3" "U7,R6,D4,L4"
+  , 610 ~=? d3p2 "R75,D30,R83,U83,L12,D49,R71,U7,L72" "U62,R66,U55,R34,D71,R55,D58,R83"
+  , 410 ~=? d3p2 "R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51" "U98,R91,D20,R16,D67,R40,U7,R15,U6,R7"
+  ]
 
 tests :: Test
-tests = TestList
-  [ 3 ~=? add 1 2
-  ]
+tests = TestList (d3tests ++ d3p1tests ++ d3p2tests)
